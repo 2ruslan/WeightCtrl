d9613881340d131d4aa7f8d73449423d4fe7d4b4
ruslan_kupchinskii <2ruslank@gmail.com>
2015-11-20 19:22:58 +0200

    db complete

--- app/src/main/java/kupchinskii/ruslan/weightctrl/DBHelper.java
+++ app/src/main/java/kupchinskii/ruslan/weightctrl/DBHelper.java
@@ -17,35 +17,6 @@ import java.util.Date;
 public class DBHelper {
 
     //region results
-    private static final String sql_sel_res_id =
-            " SELECT " + DB.C_RES_ID +
-            " FROM " + DB.T_RES +
-            " WHERE " + DB.C_RES_ONDATE + " = ?"
-            ;
-    private static final String sql_upd_res =
-            " UPDATE " + DB.T_RES + " SET " +
-            "   " + DB.C_RES_GROWTH + " = ? " +
-            "  ," + DB.C_RES_HIPS + " = ? " +
-            "  ," + DB.C_RES_WEIGHT + " = ? " +
-            " WHERE " + DB.C_RES_ID + " = ?"
-            ;
-    private static final String sql_add_res =
-            " INSERT INTO " + DB.T_RES + " ( " +
-            "  ," + DB.C_RES_ONDATE +
-            "  ," + DB.C_RES_GROWTH +
-            "  ," + DB.C_RES_HIPS +
-            "  ," + DB.C_RES_WEIGHT  +
-            " ) VALUES (?, ?, ?, ?)"
-            ;
-    private static final String sql_sel_res_all =
-            " SELECT " + DB.C_RES_ID +
-            "      , " + DB.C_RES_ONDATE +
-            "      , " + DB.C_RES_WEIGHT +
-            "      , " + DB.C_RES_HIPS +
-            "      , " + DB.C_RES_GROWTH +
-            " FROM " + DB.T_RES +
-            " ORDER BY " + DB.C_RES_ONDATE + " DESC "
-            ;
     public static void SaveResults(int pGrown, int pHips, int pWeight) {
         Cursor cursor = null;
         long id = -1;
@@ -54,8 +25,7 @@ public class DBHelper {
         SQLiteDatabase db = DB.getDBRead();
         // get current
         try {
-            cursor = db.rawQuery(sql_sel_res_id,
-                    new String[]{onDade});
+            cursor = db.query(DB.T_RES, new String[]{DB.C_RES_ID}, DB.C_RES_ONDATE + " = ?", new String[]{onDade}, null, null, null);
 
             if (cursor.getCount() > 0) {
                 cursor.moveToFirst();
@@ -66,64 +36,35 @@ public class DBHelper {
                 cursor.close();
             db.close();
         }
-        // add current
-        if (id == -1) {
-            try {
-                db = DB.getDBWrite();
-                db.execSQL(sql_add_res, new String[]{
-                        onDade,
-                        String.valueOf(pWeight),
-                        String.valueOf(pHips),
-                        String.valueOf(pGrown)
-                        });
-                ContentValues values = new ContentValues();
-                values.put(DB.C_RES_ONDATE, onDade);
 
-                id = db.insert(DB.T_RES, null, values );
-            } finally {
-                db.close();
+        ContentValues values = new ContentValues();
+        values.put(DB.C_RES_GROWTH, pGrown);
+        values.put(DB.C_RES_HIPS, pHips);
+        values.put(DB.C_RES_WEIGHT, pWeight);
+        try {
+            db = DB.getDBWrite();
+            if (id == -1) {
+                values.put(DB.C_RES_ONDATE, onDade);
+                db.insert(DB.T_RES, null, values);
             }
-        }
-        else {
-            // upd current
-            try {
-                db = DB.getDBWrite();
-                db.execSQL(sql_upd_res, new String[]{
-                                String.valueOf(pGrown),
-                                String.valueOf(pHips),
-                                String.valueOf(pWeight)}
-                );
-            } finally {
-                db.close();
-            }
+            else
+                db.update(DB.T_RES, values, DB.C_RES_ID + " = " + id, null);
+        } finally {
+            db.close();
         }
     }
 
     public static Cursor GetReusultsAll()
     {
         SQLiteDatabase db = DB.getDBRead();
-        return db.rawQuery(sql_sel_res_all, new String[]{});
+        return  db.query(DB.T_RES, new String[]{DB.C_RES_ID, DB.C_RES_ONDATE, DB.C_RES_WEIGHT
+                , DB.C_RES_GROWTH, DB.C_RES_HIPS}
+                , null, null, null, null, null);
     }
     //endregion results
 
     //region setings
-    private static final String sql_sel_stg_id =
-            " SELECT " + DB.C_STG_ID +
-                    " FROM " + DB.T_STG +
-                    " LIMIT 1"
-            ;
-    private static final String sql_add_stg =
-            " INSERT INTO " + DB.T_STG + " ( " +
-            "   " + DB.C_STG_SEX +
-            "  ," + DB.C_STG_BIRTHDAY +
-            " ) VALUES (?, ?)"
-            ;
-    private static final String sql_upd_stg =
-            " UPDATE " + DB.T_STG + " SET " +
-            "   " + DB.C_STG_SEX + " = ? " +
-            "  ," + DB.C_STG_BIRTHDAY + " = ? "
-            ;
-    public static void SaveSettings(int pSex /* 0 - male 1 - female */
+    public static void SaveSettings(int pSex /* 1 - male 0 - female */
                                    ,Date pBirthDay
                                   ) {
         Cursor cursor = null;
@@ -132,8 +73,7 @@ public class DBHelper {
         SQLiteDatabase db = DB.getDBRead();
         // get current
         try {
-            cursor = db.rawQuery(sql_sel_stg_id,
-                    new String[]{});
+            cursor =  db.query(DB.T_STG, new String[]{DB.C_STG_ID}, null, null, null, null, null);
 
             if (cursor.getCount() > 0) {
                 cursor.moveToFirst();
@@ -147,18 +87,17 @@ public class DBHelper {
 
 		ContentValues values = new ContentValues();
 		values.put(DB.C_STG_SEX, pSex);
-		values.put(DB.C_STG_BIRTHDAY, pBirthDay);
+		values.put(DB.C_STG_BIRTHDAY, DB.getDataStr(pBirthDay));
 		
-        // add
 		try {
 			db = DB.getDBWrite();
             if (id == -1) 
-				db.insert(DB.T_STG, null, values)
+				db.insert(DB.T_STG, null, values);
             else 
                 db.update(DB.T_STG, values, null, new String[]{});
-			} finally {
-                db.close();
-            }
+		} finally {
+            db.close();
+        }
     }
 
     public static settings GetSettings() {
@@ -168,8 +107,8 @@ public class DBHelper {
         SQLiteDatabase db = DB.getDBRead();
         // get current
         try {
-            cursor = db.rawQuery(sql_sel_stg_id,
-                    new String[]{});
+            cursor = db.query(DB.T_STG, new String[]{DB.C_STG_ID, DB.C_STG_BIRTHDAY, DB.C_STG_SEX}
+                    , null, null, null, null, null);
 
             if (cursor.getCount() > 0) {
                 cursor.moveToFirst();
--- app/src/main/java/kupchinskii/ruslan/weightctrl/MainActivity.java
+++ app/src/main/java/kupchinskii/ruslan/weightctrl/MainActivity.java
@@ -3,6 +3,8 @@ package kupchinskii.ruslan.weightctrl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         DB.initDB(this);
+
+        DBHelper.SaveSettings(1, new Date(10000000));
+        DBHelper.SaveResults(100, 100, 100);
+        DBHelper.GetReusultsAll();
+        DBHelper.GetSettings();
     }
 }
